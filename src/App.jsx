35d439bbffdab5abe78c05b70a4e1885d116905c
keigomichi <hundredacrewood1165g7@gmail.com>
2022-10-18 22:40:13 +0900

    feat: 商品を配列から取得できるようにする

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import Counter from "./components/Counter";
 import "./App.css";
+// import Counter from "./components/Counter";
+import ItemList from "./components/ItemList";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      {/* <Counter /> */}
+      <ItemList />
     </div>
   );
 }
---
+++ src/components/ItemList.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import Item from "./Item";
+const ItemList = () => {
+  const [items] = useState([
+    { name: "りんご", price: 300 },
+    { name: "バナナ", price: 200 },
+    { name: "いちご", price: 1000 },
+  ]);
+
+  return (
+    // ここにItemコンポーネントを並べる
+    <div>
+      <h2>商品リスト</h2>
+      <ul>
+        {items.map((item) => {
+          return (
+            <li key={item.name}>
+              <Item name={item.name} price={item.price} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default ItemList;
