9ccb3d46c450f09e39d8be751c9e1460188710b4
keigomichi <hundredacrewood1165g7@gmail.com>
2022-10-18 22:50:21 +0900

    chore: 500円以上の場合は文字を赤色にする

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,6 +1,6 @@
 const Item = (props) => {
   return (
-    <div>
+    <div style={{ color: props.price >= 500 ? "red" : "black" }}>
       <div>名前：{props.name}</div>
       <div>価格：{props.price}</div>
     </div>
